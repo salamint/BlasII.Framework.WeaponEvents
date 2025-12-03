b39760b66b0b5786ef9f798d1228b343e5c10a9e
salamint <salamint@proton.me>
2025-12-03 13:51:16 +0100

    patches: Added a patch that detects when the Mea Culpa's phantom projectile is ready

--- BlasII.Framework.WeaponEvents/Patches/MeaCulpa.cs
+++ BlasII.Framework.WeaponEvents/Patches/MeaCulpa.cs
@@ -1,5 +1,6 @@
 using HarmonyLib;
 using Il2CppTGK.Game.Components;
+using Il2CppTGK.Game.Components.Attack;
 
 namespace BlasII.Framework.WeaponEvents.Patches;
 
@@ -10,6 +11,16 @@ class MeaCulpaBerserkModeFiller_OnEnable_Patch
 	private static void Prefix(MeaCulpaBerserkModeFiller __instance)
 	{
 		Main.WeaponEventsFramework.MeaCulpaBerserkModeFiller = __instance;
+	}
+}
+
+
+[HarmonyPatch(typeof(MeaCulpaPhantomTeleportAbility), nameof(MeaCulpaPhantomTeleportAbility.ResetProjectileSpawnerAttackCooldown))]
+class MeaCulpaPhantomTeleportAbility_ResetProjectileSpawnerAttackCooldown_Patch
+{
+	private static void Prefix(MeaCulpaPhantomTeleportAbility __instance)
+	{
+		Main.WeaponEventsFramework.MeaCulpaHandlersManager.HandlePhantomProjectileReady();
 	}
 }
 
