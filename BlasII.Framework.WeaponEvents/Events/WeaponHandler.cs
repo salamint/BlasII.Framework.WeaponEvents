3b5580563e99972229b599e6709958db0d87a565
salamint <salamint@proton.me>
2025-12-03 13:39:37 +0100

    events: Documentation

--- BlasII.Framework.WeaponEvents/Events/WeaponHandler.cs
+++ BlasII.Framework.WeaponEvents/Events/WeaponHandler.cs
@@ -1,7 +1,12 @@
 namespace BlasII.Framework.WeaponEvents.Events;
 
 
-public abstract class WeaponHandler : CommonWeaponHandler
-{
-}
+/// <summary>
+/// Abstract class that handles the events of no weapon in particular, meaning
+/// whether you use Veredicto, Sarmiento y Centella, Ruego al Alba of Mea Culpa,
+/// the methods in this class will be called.
+/// The corresponding methods are automatically called by the manager.
+/// To handle these events, simply make a sub class of this class.
+/// </summary>
+public abstract class WeaponHandler : CommonWeaponHandler {}
 
