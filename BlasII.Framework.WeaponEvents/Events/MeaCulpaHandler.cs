f61fe5c8e1c2f362cf5b5cc0135d29d25b99dcc7
salamint <salamint@proton.me>
2026-08-11 11:45:56 +0200

    events: Added properties pointing to the range stat proxies instead of using the filler objects

--- BlasII.Framework.WeaponEvents/Events/MeaCulpaHandler.cs
+++ BlasII.Framework.WeaponEvents/Events/MeaCulpaHandler.cs
@@ -13,31 +13,13 @@ namespace BlasII.Framework.WeaponEvents.Events;
 public abstract class MeaCulpaHandler : CommonWeaponHandler
 {
 	/// <summary>
-	/// A shortcut to the common global MeaCulpaBerserkModeFiller reference.
+	/// Proxy to access and modify the valeu of the Berserk Mode stat.
 	/// </summary>
+	public RangeStatProxy BerserkMode { get => WeaponEventsFramework.MeaCulpaBerserkMode; }
 
 	/// <summary>
-	/// A shortcut to the common global current berserk mode value.
-	/// The getter and setter call the get and set methods of the player stats.
+	/// A shortcut to the common global MeaCulpaBerserkModeFiller reference.
 	/// </summary>
-	public int CurrentBerserkModeValue
-	{
-		get
-		{
-			if (BerserkModeFiller != null)
-			{
-				return BerserkModeFiller.stats.GetCurrentValue(BerserkModeFiller.berserkStat);
-			}
-			return 0;
-		}
-		set
-		{
-			if (BerserkModeFiller != null)
-			{
-				BerserkModeFiller.stats.SetCurrentValue(BerserkModeFiller.berserkStat, value);
-			}
-		}
-	}
 	public MeaCulpaBerserkModeFiller? BerserkModeFiller { get => Main.WeaponEventsFramework.MeaCulpaBerserkModeFiller; }
 
 	/* Combo attacks */
